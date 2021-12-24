441d8bd56affa3cc426615ad9bef2dd5d2c2eb20
Inet Class User <inet@CIR.LAN>
2021-12-24 19:03:12 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import javax.naming.PartialResultException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+    public class Main {
+        private static Object String;
+
+        public static void main(String[] args) {
+            Integer wis[] = new Integer[10];
+            System.out.print(" \n Your numbers is: \n");
+            {
+            for (int i = 0; i < wis.length; i++) {
+                wis[i] = (int) (Math.random() * 10);
+                System.out.print(wis[i] + " and ");}
+            System.out.print("\n Oh, my imagination is empty. Ahahah. Lol. What? I serious.");
+                {
+                    Scanner sc = new Scanner(System.in);
+                    int number = sc.nextInt();
+                    String killme[] = new String[100]
+                    String string = Integer.toString(number);
+                    System.out.println("What? You're type " + number + "???");
+                    System.out.print("Shut up!");
+                }
+
+
+            }
+        }
+    }
